9e2dcad8057013c7b2686995bc76906b7c56a880
sam3ds <sam3ds@gmail.com>
2022-08-05 05:29:18 +0300

    11-11-43

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,4 +1,18 @@
 (function($) {
+    "use strict"
+
+    if($('.mobile-menu').length){
+        var mobileMenuContent = $('#top-navigation .navigation').html();
+
+        $('.mobile-menu .navigation').append(mobileMenuContent);
+
+        $('.sticky-header .navigation').append(mobileMenuContent);
+
+        $('.mobile-menu .close-btn').on("click", function() {
+            $('body').removeClass('mobile-menu-visible');
+        });
+    }
+
 
 })(jQuery);
 
